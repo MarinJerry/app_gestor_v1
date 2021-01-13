921876dc3f7262ee1f4ed9d1ade1bc54b11be229
MarinJerry <46719214+MarinJerry@users.noreply.github.com>
2021-01-13 17:09:52 -0600

    2021-11-03 JM: Se crea vista que obtiene una lista desde una petici[on GET y la muestra con el widget List<View>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 // import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -171,10 +174,11 @@ class _MyHomePageState extends State<MyHomePage> {
                     textColor: Colors.white,
                     color: Colors.deepOrange[300],
                     child: const Text('Sign in'),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        _register();
-                      }
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SecondRoute()),
+                      );
                     },
                   )),
               Container(
@@ -214,21 +218,71 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class _RegisterEmailSection extends StatefulWidget {
-  final String title = 'Registration';
-  @override
-  State<StatefulWidget> createState() => _RegisterEmailSectionState();
-}
-
-class _RegisterEmailSectionState extends State<_RegisterEmailSection> {
-  /*Properties */
-
+class SecondRoute extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return new MaterialApp(
+      title: 'Fetch Data Example',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Fetch Data Example'),
+        ),
+        body: new Container(
+          child: new FutureBuilder<List<User>>(
+            future: fetchUsersFromGitHub(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return new ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      return new Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            new Text(snapshot.data[index].name,
+                                style:
+                                    new TextStyle(fontWeight: FontWeight.bold)),
+                            new Divider()
+                          ]);
+                    });
+              } else if (snapshot.hasError) {
+                return new Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner
+              return new CircularProgressIndicator();
+            },
+          ),
+        ),
       ),
     );
   }
+
+  Future<List<User>> fetchUsersFromGitHub() async {
+    final response = await http.get('https://api.github.com/users');
+    print(response.body);
+    List responseJson = json.decode(response.body.toString());
+    List<User> userList = createUserList(responseJson);
+    return userList;
+  }
+
+  List<User> createUserList(List data) {
+    List<User> list = new List();
+    for (int i = 0; i < data.length; i++) {
+      String title = data[i]["login"];
+      int id = data[i]["id"];
+      User user = new User(name: title, id: id);
+      list.add(user);
+    }
+    return list;
+  }
+}
+
+class User {
+  String name;
+  int id;
+  User({this.name, this.id});
 }
